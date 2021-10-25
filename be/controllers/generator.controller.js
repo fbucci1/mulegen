d10227a827c5d24f874b33c6a1efd99705cd6c9b
FernandoBucci <fernando.bucci@capgemini.com>
2021-10-25 23:27:48 +0200

    -

--- be/controllers/generator.controller.js
+++ be/controllers/generator.controller.js
@@ -1,85 +1,88 @@
-/*
-const { generateTemplateFilesBatch  } = require('generate-template-files');
-
-function generateFiles(params){
-  generateTemplateFilesBatch([
-    {
-      option: 'Generate all templates',
-      defaultCase: '(pascalCase)',
-      entry: {
-        folderPath: './templates/',
-      },
-      dynamicReplacers: params,
-      output: {
-        path: './generated',
-        pathAndFileNameDefaultCase: '(kebabCase)',
-        overwrite: true,
-      },
-      onComplete: (results) => {
-        console.log(`results`, results);
-      },
-    },
-  ]).catch(() => {
-      console.log('Build Error');
-    });
-}
-//
-params=[
-  { slot: '__store__', slotValue: 'sss' },  
-  { slot: '__model__', slotValue: 'mmm' },
-];
-generateFiles(params);
-//
-*/
-
 var ejs = require('ejs');
 var fs = require('fs');
 var path = require('path');
+var jp = require('jsonpath');
 
-function generateFiles(templateName, params){
-  var __templateDirName='./templates'; // templateName
-  var __generatedDirName='./generated/';
-  generateFilesInDir(__templateDirName, __generatedDirName, params);
+function generateFiles(params){
+  for(var i=0;i<params.executions.length;i++){
+    var template=params.executions[i].template; 
+    var jsonpath=params.executions[i].jsonpath; 
+    //
+    var __templateDirName='./templates/'+template;
+    var __generatedDirName='./generated/';
+    //
+    var valuesIterator=jp.query(params.values, jsonpath);
+    for(var ii=0;ii<valuesIterator.length;ii++){
+      var values=valuesIterator[ii];
+      generateFilesInDir(__templateDirName, __generatedDirName, values);
+    }
+  }
 }
 
-function generateFilesInDir(__templateDirName, __generatedDirName, params){
+function generateFilesInDir(__templateDirName, __generatedDirName, values){
   var files=fs.readdirSync(__templateDirName);
   for(var i=0;i<files.length;i++){
-    var filename=path.join(__templateDirName,files[i]);
-    var stat = fs.lstatSync(filename);
+    var srcFilename=path.join(__templateDirName,files[i]);
+    var tgtFilename=path.join(__generatedDirName,files[i]);
+    var stat = fs.lstatSync(srcFilename);
     if (stat.isFile()){
       if (files[i].endsWith('.ejs')){
-        generateFile(__templateDirName, files[i], __generatedDirName, params);        
+        generateFile(__templateDirName, files[i], __generatedDirName, values);        
+      } else if (files[i]=='.gitignore'){
+        console.log('Skipping '+srcFilename);
       } else {
-        console.log('Skipping '+filename);
+        console.log('Copying '+srcFilename);
+        fs.copyFileSync(srcFilename, tgtFilename);
       }
     }
     if (stat.isDirectory()){
-      generateFilesInDir(filename, path.join(__generatedDirName,files[i]), params);        
+      generateFilesInDir(srcFilename, tgtFilename, values);        
     }
   };
 }
 
-function generateFile(templateDirName, templateFileName, outputFolder, params){
-  var templateFilePath = templateDirName + '/' + templateFileName;
-  console.log("Applying template: "+templateFilePath);
-  const values = params.values;
-  var template = fs.readFileSync(templateFilePath, 'utf8');
+function generateFile(templateDirName, templateFileName, outputFolder, values){
+  var srcFilename=path.join(templateDirName,templateFileName);
+  var tgtFilename=path.join(outputFolder,templateFileName.substring(0,templateFileName.length-4));
+  console.log("Applying template: "+srcFilename);
+  console.log('  Values: '+JSON.stringify(values));
+  var template = fs.readFileSync(srcFilename, 'utf8');
   var renderized = ejs.render(template, values);
   //console.log(renderized);
-  var outputFilename=path.join(outputFolder,templateFileName.substring(0,templateFileName.length-4));
   if (!fs.existsSync(outputFolder)){
     console.log("  Creating folder     "+outputFolder);
     fs.mkdirSync(outputFolder, { recursive: true });
   }
-  console.log("  Writing file     "+templateFilePath+"->"+outputFilename);
-  fs.writeFileSync(outputFilename, renderized, 'utf8');
+  console.log("  Writing file     "+srcFilename+"->"+tgtFilename);
+  fs.writeFileSync(tgtFilename, renderized, 'utf8');
   console.log("  Written successfully...");
 }
 
-var templateName='template1'; 
-var params = {'values': {xxx: 'Hello World!'}};
-generateFiles(templateName, params);
+var params = {
+  'executions': [
+    {
+      'template': 'template1/Int',
+      'jsonpath': '$',
+    },
+    {
+      'template': 'template1/IntFlow',
+      'jsonpath': '$.IntFlows[*]',
+    }
+  ],
+  'values': 
+    {
+      'IntName': 'TheApp1',
+      'IntFlows': [
+        {
+          'FlowName': 'Flow11',
+        },
+        {
+          'FlowName': 'Flow12',
+        },
+      ],
+    }
+  };
+generateFiles(params);
 
 exports.generate = (req, res) => {
   res.status(200).end('{"status:":"ok"}').send();
